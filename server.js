3f1371781d73a24ad4190d2df9688c6a631d7599
Howe Huang <shihuihzh@gmail.com>
2024-08-06 22:44:59 +0800

    add test

--- server.js
+++ server.js
@@ -43,6 +43,32 @@ fastify.get(`/:path/:rid`, async (request, reply) => {
     }
 })
 
+fastify.post('/test/:params', function (request, reply) {
+    console.log(request.body)
+    console.log(request.query)
+    console.log(request.params)
+    console.log(request.headers)
+    console.log(request.raw)
+    console.log(request.server)
+    console.log(request.id)
+    console.log(request.ip)
+    console.log(request.ips)
+    console.log(request.hostname)
+    console.log(request.protocol)
+    console.log(request.url)
+    console.log(request.routeOptions.method)
+    console.log(request.routeOptions.bodyLimit)
+    console.log(request.routeOptions.method)
+    console.log(request.routeOptions.url)
+    console.log(request.routeOptions.attachValidation)
+    console.log(request.routeOptions.logLevel)
+    console.log(request.routeOptions.version)
+    console.log(request.routeOptions.exposeHeadRoute)
+    console.log(request.routeOptions.prefixTrailingSlash)
+    console.log(request.routeOptions.logLevel)
+    request.log.info('some info')
+  })
+
 const start = async () => {
     itv.setup(fastify)
     try {
